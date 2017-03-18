73609f54b8f894a2b2cd99542bb10b41b27c562e
Manu Rosa <manurosa11@gmail.com>
2017-03-18 13:04:21 +0100

    Solve problem 2

--- test/test.js
+++ test/test.js
@@ -1,9 +1,16 @@
 var assert = require('assert')
 
-var problem1 = require('../problems/problem001')
+var problem1 = require('../js/problem001')
+var problem2 = require('../js/problem002')
 
 describe('Problem 001', function () {
   it('should return 233168 as the correct answer', function () {
     assert.equal(233168, problem1.run(1000))
+  })
+})
+
+describe('Problem 002', function () {
+  it('should return 4613732 as the correct answer', function () {
+    assert.equal(4613733, problem2.run(4000000))
   })
 })
